0c8023e565b207cbe0a028f4f71be9cb5c70fcb1
lifeng-yin <lifeng-yin@proton.me>
2022-12-10 17:43:39 -0500

    Add prototype logo, separate scss into files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import './Navbar.scss';
 
 const Navbar = () => {
     return (
         <nav>
-            <div class="logo">
-                <Link key='Landing' class="logo" to="/">Nim</Link>
+            <div className="logo">
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="#5F82FF" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 3h8v26H0zm12 0h8v26h-8zm12 0h8v26h-8z"/>
+                </svg>
+                <Link key='Landing' to="/">Nim</Link>
             </div>
-            <div class="buttons">
+            <div className="buttons">
                 <Link className="secondary-button" key="Login" to="/login">Login</Link>
                 <Link className="primary-button" key="Register" to="/register">Register</Link>
             </div>
