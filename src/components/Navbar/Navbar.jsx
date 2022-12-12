bad13382e6869debef88db2fe30966e27c7f40ad
lifeng-yin <lifeng-yin@proton.me>
2022-12-11 20:20:24 -0500

    Minor changes to navbar logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,12 @@ import './Navbar.scss';
 const Navbar = () => {
     return (
         <nav>
-            <div className="logo">
+            <Link key="Landing" to="/" className="logo">
                 <svg width="32" height="32" viewBox="0 0 32 32" fill="#5F82FF" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 3h8v26H0zm12 0h8v26h-8zm12 0h8v26h-8z"/>
                 </svg>
-                <Link key='Landing' to="/">Nim</Link>
-            </div>
+                <span>Nim</span>
+            </Link>
             <div className="buttons">
                 <Link className="secondary-button" key="Login" to="/login">Login</Link>
                 <Link className="primary-button" key="Register" to="/register">Register</Link>
