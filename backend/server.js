4f96e7f932b3b1aad38b507e745ff17a4083950e
lifeng-yin <lifeng-yin@proton.me>
2022-12-13 21:45:01 -0500

    Update frontend package.json, fix style in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,11 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+  res.send('Hello World!');
+});
 
 server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is listening at http://localhost:${process.env.PORT || 3000} 🚀`)
-})
+  console.log(
+    `Server is listening at http://localhost:${process.env.PORT || 3000} 🚀`
+  );
+});
