3ad51d7db3ac58087474a45a22663e6c4717e440
lifeng-yin <lifeng-yin@proton.me>
2022-12-13 21:41:37 -0500

    Add hello world with express

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening at http://localhost:${process.env.PORT || 3000} 🚀`)
+})
